41d5de4c3d8e18066158a788b0b9624a7a61619e
pushthat <matias.berny@yahoo.fr>
2018-10-21 18:44:53 +0200

    Update plan.sh

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -14,7 +14,7 @@
 
 pkg_name=sample-node-app
 pkg_origin=pushthat
-pkg_version="1.1.2"
+pkg_version="1.2.2"
 pkg_deps=(core/node)
 
 # Habitat provides you with a number of built-in "callbacks" to use
